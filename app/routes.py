941fe051510cb19aa344c4325cf7928a1b9e0a92
Developer_Web_PY <Developer_PY@fifa.com>
2026-06-29 20:23:32 -0500

    Creacion de formulario contacto

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/contacto", methods=["GET"])
+def contacto():
+    nombre = request.args.get("nombre", "")
+    mensaje = request.args.get("mensaje", "")
+    return render_template(
+        "index.html",
+        nombre=nombre,
+        mensaje=mensaje,
+        request_ip=request.remote_addr,
+        script_path=os.path.abspath(__file__)
+    )
